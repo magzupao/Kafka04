dee746c7d56dfad27154c80f9fdae0da0f4767b0
magzupao <mguado@gmail.com>
2023-01-05 20:54:42 +0100

    first commit

---
+++ src/main/java/com/refactorizando/kafka/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package com.refactorizando.kafka.controller;
+
+import com.refactorizando.kafka.kafka.producer.KafkaProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaController {
+
+  private KafkaProducer kafkaProducer;
+
+  public KafkaController(KafkaProducer kafkaProducer) {
+    this.kafkaProducer = kafkaProducer;
+  }
+
+  // http:localhost:8080/api/v1/kafka/publish?message=hello world
+  @GetMapping("/publish")
+  public ResponseEntity<String> publish(@RequestParam("message") String message){
+    kafkaProducer.sendMessage(message);
+    return ResponseEntity.ok("Message sent to the topic");
+  }
+}
