84fc3fda1eaa30fd6934412d773202d598d2fea4
magzupao <mguado@gmail.com>
2023-01-06 11:36:01 +0100

    igualamos con kafka-spring-boot

--- src/main/java/com/refactorizando/kafka/controller/KafkaController.java
+++ src/main/java/com/refactorizando/kafka/controller/KafkaController.java
@@ -3,10 +3,7 @@ package com.refactorizando.kafka.controller;
 import com.refactorizando.kafka.kafka.producer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
